bf3ddce9556ccc84c74d13ccfa59188b637819ce
Marcelo Martins Da Silva <154945646+MarceloSilva25@users.noreply.github.com>
2026-02-24 21:21:37 -0300

    Sistema de estoque simples

---
+++ Sistema de estoque/src/Main.java
@@ -0,0 +1,9 @@
+// Para compilar e executar a partir do terminal, use:
+// javac Main.java && java Main
+
+public class Main {
+    public static void main(String[] args) {
+    import java.util.ArrayList;
+
+    }
+}
